94b630fa28e8ca60f310b77532a564fd0845d721
s21696 <s21696@pjwstk.edu.pl>
2024-03-20 21:33:35 +0100

    Modyfikacja 2

--- src/Pjatk.Apbd/Program.cs
+++ src/Pjatk.Apbd/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Czy będzie problemem jeśli pod przyszłe zadania zapomnę zmienić maila na commitach na uczelniany? Nie chcę zmieniać globalnych ustawień");
+﻿Console.WriteLine(
+    "Czy będzie problemem jeśli pod przyszłe zadania zapomnę zmienić maila na commitach na uczelniany? Nie chcę zmieniać globalnych ustawień"
+);
