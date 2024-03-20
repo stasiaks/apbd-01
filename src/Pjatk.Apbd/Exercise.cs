1bfa3225b8a536a88be266a4ef6e217b931bc989
s21696 <s21696@pjwstk.edu.pl>
2024-03-20 23:43:04 +0100

    Jaka znowu petla?

--- src/Pjatk.Apbd/Exercise.cs
+++ src/Pjatk.Apbd/Exercise.cs
@@ -3,7 +3,7 @@ namespace Pjatk.Apbd;
 public static class Exercise
 {
     /// <summary> Wszyscy kochają pisać własne, gorsze wersje System.Linq.Enumerable.Average </summary>
-    public static double Average(int[] values) => (double)values.Sum() / values.Length;
+    public static double Average(int[] values) => (double)values.Sum() / values.Count();
 
     /// <summary> Wszyscy kochają pisać własne, gorsze wersje System.Linq.Enumerable.Max </summary>
     public static int Max(int[] values) => values.Aggregate((acc, next) => acc > next ? acc : next);
