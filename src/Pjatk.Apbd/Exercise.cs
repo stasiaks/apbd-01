718668e06883ef5fef0fc35949ea2e7ec28f8476
s21696 <s21696@pjwstk.edu.pl>
2024-03-20 23:47:14 +0100

    formatting

--- src/Pjatk.Apbd/Exercise.cs
+++ src/Pjatk.Apbd/Exercise.cs
@@ -3,7 +3,8 @@ namespace Pjatk.Apbd;
 public static class Exercise
 {
     /// <summary> Wszyscy kochają pisać własne, gorsze wersje System.Linq.Enumerable.Average </summary>
-    public static double Average(int[] values) => values.Aggregate(0d, (acc, next) => acc + next) / values.Count();
+    public static double Average(int[] values) =>
+        values.Aggregate(0d, (acc, next) => acc + next) / values.Count();
 
     /// <summary> Wszyscy kochają pisać własne, gorsze wersje System.Linq.Enumerable.Max </summary>
     public static int Max(int[] values) => values.Aggregate((acc, next) => acc > next ? acc : next);
